e0982e89205a620c2099a89c5ee3b45451e0725b
alex-851 <99877694+alex-851@users.noreply.github.com>
2022-08-08 21:17:52 +0530

    Update BANK.java

--- BANK.java
+++ BANK.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class BANK {
 		HI THERE
+			how are you
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi there,how many credit cards are there with you?");
